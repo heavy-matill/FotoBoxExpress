aa96849fc5acc1912b2e2aea116c49174be502bc
heavy-matill <mathe23491@gmx.de>
2018-05-15 16:37:43 +0200

    added logging for gpio4

--- controllers/gpioController.js
+++ controllers/gpioController.js
@@ -11,6 +11,7 @@ gpio4.on("change", function(val) {
 	// value will report either 1 or 0 (number) when the value changes 
 	console.log("gpio is ", val);
 	if(val){
+		console.log("pausing thumbnail creation queue due to io interrupt");
 		fotoBoxController.stopQueue();
 	}
 });
