1e84f807a3426c14da5a7aa4ece0f523d4a35824
heavy-matill <heavymatill@gmail.com>
2019-02-18 15:59:15 +0100

    more commenmts on ow to process the grey scale image

--- controllers/printerController.js
+++ controllers/printerController.js
@@ -5,7 +5,7 @@
 // install 
 /*
 sudo apt-get update
-sudo apt-get install libcups2-dev libcupsimage2-dev git build-essential cups system-config-printer
+sudo apt-get install libcups2-dev libcupsimage2-dev git build-essential cups //system-config-printer
 
 git clone https://github.com/adafruit/zj-58
 cd zj-58
@@ -13,10 +13,22 @@ make
 sudo ./install
 
 sudo lpadmin -p 58mmThermal -E -v serial:/dev/serial0?baud=9600 -m zjiang/ZJ-58.ppd
+
+disable login over uart
+enabe uart hardware
 */
 
 /*
 print
 lp- d 58mmThermal -o portrait -o fit-to-page imagexy.jpg
 echo "filename" | lp -d 58mmThermal
+*/
+/*
+Print thumbnails
+queue until thumbnail exists
+*/
+/*
+http://www.imagemagick.org/Usage/color_mods/#normalize
+    convert src.jpg -colorspace gray dest.jpg
+  convert gray_range.jpg  -contrast-stretch 10%  stretch_gray.jpg
 */
