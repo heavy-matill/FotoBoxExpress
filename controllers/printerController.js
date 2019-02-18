e0e3cdc165cec0dc3e3b497df87d24b0cfa863f9
heavy-matill <heavymatill@gmail.com>
2019-02-18 11:35:46 +0100

    added printer controller comments

---
+++ controllers/printerController.js
@@ -0,0 +1,22 @@
+// check if available 
+// sudo lpstat -a 58mmThermal
+// asser response!= "lpstat: Invalid destination in list "58mmThermal".
+
+// install 
+/*
+sudo apt-get update
+sudo apt-get install libcups2-dev libcupsimage2-dev git build-essential cups system-config-printer
+
+git clone https://github.com/adafruit/zj-58
+cd zj-58
+make
+sudo ./install
+
+sudo lpadmin -p 58mmThermal -E -v serial:/dev/serial0?baud=9600 -m zjiang/ZJ-58.ppd
+*/
+
+/*
+print
+lp- d 58mmThermal -o portrait -o fit-to-page imagexy.jpg
+echo "filename" | lp -d 58mmThermal
+*/
