71609d387629d1f4cd7276d88601471000fdf713
heavy-matill <heavymatill@gmail.com>
2019-02-20 10:09:27 +0100

    using key of json boject i nnew foto

--- routes/newfoto.js
+++ routes/newfoto.js
@@ -1,20 +1,12 @@
 var express = require('express');
 var fotoBoxController = require('../controllers/fotoBoxController');
 var router = express.Router();
-var bodyParser = require('body-parser');
 
 /* GET home page. */
-/*router.get('/:folder/:file', function(req, res, next) {
-	console.log('File available: ' + req.params.folder + 'and /' + req.params.file);
-	fotoBoxController.downloadNewFoto(req.params.folder,req.params.file);
-	res.end('File available: ' + req.params.folder + '/' + req.params.file);
-});*/
 router.post('/', function(req, res, next) {	
 	const body = req.body;
-	console.log(req)
-	console.log(body)
-	console.log("new image available at: " + body.imageUrl)
-	fotoBoxController.downloadNewFoto(body.imageUrl);
+	let imageUrl = Object.keys(body)[0]
+	fotoBoxController.downloadNewFoto(imageUrl);
 	res.end()
 });
 
