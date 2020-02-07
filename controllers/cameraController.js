c3ac59e5d821050a6bbcd925d44bee00f190b24b
heavy-matill <heavymatill@gmail.com>
2020-02-07 18:02:35 +0100

    increased logging

--- controllers/cameraController.js
+++ controllers/cameraController.js
@@ -3,7 +3,7 @@ var gphoto2 = require('gphoto2');
 var GPhoto = new gphoto2.GPhoto2();
 
 // Negative value or undefined will disable logging, levels 0-4 enable it.
-GPhoto.setLogLevel(1);
+GPhoto.setLogLevel(4);
 GPhoto.on('log', function (level, domain, message) {
     console.log(domain, message);
 });
@@ -23,47 +23,13 @@ async function takePicture() {
         camera.setConfigValue('capturetarget', 1, function (er) {
           //...
         });*/
-
-        /*// Take picture with camera object obtained from list()
-        camera.takePicture({download: true}, function (er, data) {
-          fs.writeFileSync(__dirname + '/picture.jpg', data);
-        });*/
-
-        /*// Take picture and keep image on camera
-        camera.takePicture({
-          download: true,
-          keep: true
-        }, function (er, data) {
-          fs.writeFileSync(__dirname + '/picture.jpg', data);
-        });
-       
-        // Take picture without downloading immediately
-        camera.takePicture({download: false}, function (er, path) {
-          console.log(path);
-        });*/
-
+        
         // Take picture and download it to filesystem
-        camera.takePicture({
+        await camera.takePicture({
             targetPath: '/tmp/foo.XXXXXX'
         }, function (er, tmpname) {
             fs.renameSync(tmpname, __dirname + '/picture.jpg');
         });
-
-        /*// Download a picture from camera
-        camera.downloadPicture({
-          cameraPath: '/store_00020001/DCIM/100CANON/IMG_1231.JPG',
-          targetPath: '/tmp/foo.XXXXXX'
-        }, function (er, tmpname) {
-          fs.renameSync(tmpname, __dirname + '/picture.jpg');
-        });
-       
-        // Get preview picture (from AF Sensor, fails silently if unsupported)
-        camera.takePicture({
-          preview: true,
-          targetPath: '/tmp/foo.XXXXXX'
-        }, function (er, tmpname) {
-          fs.renameSync(tmpname, __dirname + '/picture.jpg');
-        });*/
     });
 }
 
