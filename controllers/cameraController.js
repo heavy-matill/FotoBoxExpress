db0f0220d0456fb1b54583aca7fcf5a6d445addb
heavy-matill <heavymatill@gmail.com>
2020-02-07 21:14:13 +0100

    removed gphoto2

--- controllers/cameraController.js
+++ controllers/cameraController.js
@@ -1,17 +1,10 @@
 var fs = require('fs');
-//var gphoto2 = require('gphoto2');
 var exec = require('child_process').exec;
 var waitOn = require('wait-on');
 var GPhoto = new gphoto2.GPhoto2();
 
 var nconf = require('nconf');
 
-
-// Negative value or undefined will disable logging, levels 0-4 enable it.
-GPhoto.setLogLevel(1);
-GPhoto.on('log', function (level, domain, message) {
-    console.log(domain, message);
-});
 var fileName = nconf.get('Paths:localFotos') + '/picture2.jpg'
 exports.takePicture = async function () {
     exec('gphoto2 --capture-image-and-download --filename="'+ fileName +'"', function callback(error, stdout, stderr) {
