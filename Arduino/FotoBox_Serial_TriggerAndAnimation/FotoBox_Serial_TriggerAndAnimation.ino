80ab55e9af0081001e4083502d9656f4bb58d40d
heavy-matill <heavymatill@gmail.com>
2021-03-04 15:40:15 +0100

    added animation

--- Arduino/FotoBox_Serial_TriggerAndAnimation/FotoBox_Serial_TriggerAndAnimation.ino
+++ Arduino/FotoBox_Serial_TriggerAndAnimation/FotoBox_Serial_TriggerAndAnimation.ino
@@ -1,31 +1,51 @@
 #include <CommandParser.h>
+#include <FastLED.h>
 
 typedef CommandParser<> MyCommandParser;
 
 MyCommandParser parser;
+
+#define pinR D8
+#define pinG D6
+#define pinB D7
+
+int inByte = 0;         // incoming serial byte
+int32_t tAnim = 0;
+int32_t tLeft = 0;
+int32_t tAnimStart = 0;
+
+const byte ledPin = LED_BUILTIN;
+const byte interruptPin = D2;
+uint8_t state = 0;
+
+uint8_t pinsRGB[3] = {pinR, pinG, pinB};
+CRGB colShutter[3] = {CRGB::Red, CRGB::Yellow, CRGB::Green};
 
 void cmd_test(MyCommandParser::Argument *args, char *response) {
   Serial.print("test string: "); Serial.println(args[0].asString);
 }
 
 void cmd_count(MyCommandParser::Argument *args, char *response) {
-  Serial.print("countdown ms uint64: "); Serial.println((uint32)args[0].asUInt64);
+  tAnim = (uint32) args[0].asUInt64;
+  tAnimStart = millis();
+  state = 2;
+  Serial.print("countdown time in ms: "); Serial.println(tAnim);
 }
-void cmd_stdby(MyCommandParser::Argument *args, char *response) {  
+void cmd_stdby(MyCommandParser::Argument *args, char *response) { 
+  state = 1; 
   Serial.println("switched to standby");
 }
 
 void cmd_off(MyCommandParser::Argument *args, char *response) {
+  state = 0;
   Serial.println("switched off");
 }
 
-
-int inByte = 0;         // incoming serial byte
-uint8_t tAnim = 0;
-
-const byte ledPin = LED_BUILTIN;
-const byte interruptPin = D2;
-uint8_t state = 0;
+void paintLed(CRGB col) {
+  for( int i = 0; i<3; i++) {
+    analogWrite(pinsRGB[i], col.raw[i]);
+  }   
+}
 
 void setup() {
   // start serial port at 9600 bps:
@@ -38,7 +58,10 @@ void setup() {
   parser.registerCommand("COUNTDOWN", "u", &cmd_count);  
   parser.registerCommand("STANDBY", "", &cmd_stdby);  
   parser.registerCommand("OFF", "", &cmd_off);
-  
+
+  for( int i = 0; i<3; i++) {    
+    pinMode(pinsRGB[i], OUTPUT);
+  }
   pinMode(ledPin, OUTPUT);
   pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), triggerButton, CHANGE);
@@ -46,8 +69,7 @@ void setup() {
 
 void loop() {
   proc_cmd();
-  // output trigger state
-  digitalWrite(ledPin, state);
+  proc_anim();
 }
 
 void proc_cmd() {
@@ -62,6 +84,49 @@ void proc_cmd() {
   }
 }
 
+void proc_anim() {
+  switch(state) {
+    case 0:    
+      digitalWrite(ledPin, 1);
+      paintLed(CRGB::Black); 
+      break;
+    case 1:
+      digitalWrite(ledPin, 0);
+      paintLed(CRGB::Black); 
+      break;
+    case 2:
+      proc_count();
+      break;
+    default:
+      break;
+  }
+}
+
+
+void proc_count() {
+  tLeft =  tAnimStart +tAnim - millis();
+   
+  if (tLeft<=0) {
+    state = 1;
+    return;
+  }
+  //uint8_t numLed = map(tLeft, 0, tAnim, 0, 2);
+  uint8_t numLed = 2-tLeft*3/tAnim;
+  paintLed(colShutter[numLed]);
+  //Serial.print(tLeft); Serial.print(" "); Serial.println(numLed);
+  /*for(int col = 0; col < (sizeof(colShutter)/sizeof(colShutter[0])); col++) { 
+    fill_solid(leds, NUM_LEDS, CRGB::Black);   
+    for(int dot = 0; dot < NUM_LEDS; dot++) { 
+      leds[dot] = colShutter[col];
+      FastLED.show();
+      // clear this led for the next time around the loop
+      delay(tiDelay/(NUM_LEDS*3));
+      animateAF();
+    }  
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
+    
+  }*/
+}
 
 ICACHE_RAM_ATTR void triggerButton() {
   Serial.print("TRIGGER ");
