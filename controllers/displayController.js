1128997f6a2b6f53fbc14e68283561c5678dde82
Mathias Illian <heavymatill@gmail.com>
2022-02-12 18:27:13 +0100

    moved chromium start to autostart

--- controllers/displayController.js
+++ controllers/displayController.js
@@ -2,8 +2,8 @@ var config = require('../config')
 const shellExec = require('shell-exec')
 
 exports.init = async function() {
-    // kill all chrome apps    
-    shellExec('killall chromium').then(value => console.log(value.stdout + value.stderr));
-    shellExec('chromium --user-data-dir=/home/pi/FotoBoxExpress/chromium_user/temp1 --window-position=0,0 --new-window --kiosk http://localhost:8000/fotobox').then(value => console.log(value.stdout + value.stderr)); // --user.data.dir=temp1 
-    shellExec('chromium --user-data-dir=/home/pi/FotoBoxExpress/chromium_user/temp2 --window-position=2000,0 --new-window --kiosk http://localhost:8000/gallery').then(value => console.log(value.stdout + value.stderr));
+    // kill all chrome apps not necessary, chrome is started in autostart, since as of now its not possible to start chromium on display from nodejs
+    //shellExec('killall chromium').then(value => console.log(value.stdout + value.stderr));
+    //shellExec('chromium --user-data-dir=/home/pi/FotoBoxExpress/chromium_user/temp1 --window-position=0,0 --new-window --kiosk http://localhost:8000/fotobox').then(value => console.log(value.stdout + value.stderr)); // --user.data.dir=temp1 
+    //shellExec('chromium --user-data-dir=/home/pi/FotoBoxExpress/chromium_user/temp2 --window-position=2000,0 --new-window --kiosk http://localhost:8000/gallery').then(value => console.log(value.stdout + value.stderr));
 }
