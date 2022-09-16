84653962ad97d9bbd9c60f7ec926eec7261f41d1
Mathias Illian <heavymatill@gmail.com>
2022-09-16 15:04:42 +0200

    reinitialize serial controller if its not open

--- controllers/serialController.js
+++ controllers/serialController.js
@@ -4,55 +4,66 @@ const fotoBoxController = require('./fotoBoxController');
 
 const SerialPort = require('serialport')
 
-var port;
+var port = { isOpen: false };
 exports.readyState = 0;
-exports.init = async function () {
-    console.log('RxSer initializing');
-    port = new SerialPort(config.get("Serial:Path"), { //config.get("Serial:Path")
-        baudRate: 9600
-    }, function (error) {
-        if (error) {
-            console.log('RxSer Error:')
-            console.log(error)
-            console.log("RxSer try following paths:")
-            SerialPort.list().then(function (ports) {
-                ports.forEach(function (port) {
-                    if (port.pnpId)
-                        console.log(port)
-                })
-            });
-        } else {
-            console.log('RxSer connected')
-            exports.readyState = 1;
-        }
-        port.on('readable', function () {
-            let temp = port.read().toString();
-            if (temp.match('trigger')) {
-                cameraController.triggerCamera();
-            }
-            console.log('RxSer: ' + temp);
-        })
-        port.on('close', function () {
-            console.log('RxSer closed');
-            exports.readyState = 0;
-        })
-    })
-};
-exports.init();
+exports.init = async function() {
+    if (!port.isOpen) {
+        console.log('RxSer initializing');
 
-exports.testCommand = async function (strTest) {
+        SerialPort.list().then((ports) => {
+            for (let port_struct of ports) {
+                if (port_struct.path.startsWith('/dev/ttyUSB')) {
+                    port = new SerialPort(port_struct.path, { //config.get("Serial:Path")
+                        baudRate: 9600
+                    }, function(error) {
+                        if (error) {
+                            console.log('RxSer Error:')
+                            console.log(error)
+                            console.log("RxSer try following paths:")
+                            SerialPort.list().then(function(ports) {
+                                ports.forEach(function(port) {
+                                    if (port.pnpId)
+                                        console.log(port)
+                                })
+                            });
+                        } else {
+                            console.log('RxSer connected')
+                            exports.readyState = 1;
+                        }
+                        port.on('readable', function() {
+                            let temp = port.read().toString();
+                            if (temp.match('trigger')) {
+                                cameraController.triggerCamera();
+                            }
+                            console.log('RxSer: ' + temp);
+                        })
+                        port.on('close', function() {
+                            console.log('RxSer closed');
+                            exports.readyState = 0;
+                        })
+                    })
+                }
+            }
+        })
+    }
+};
+exports.init()
+setInterval(exports.init, 20000);
+
+
+exports.testCommand = async function(strTest) {
     port.write('TEST ' + strTest + '\n')
 }
 
-exports.countdownCommand = async function (tiAnim) {
+exports.countdownCommand = async function(tiAnim) {
     port.write('COUNTDOWN ' + tiAnim + '\n')
 }
-exports.standbyCommand = async function () {
+exports.standbyCommand = async function() {
     port.write('STANDBY' + '\n')
 }
-exports.offCommand = async function () {
+exports.offCommand = async function() {
     port.write('OFF' + '\n')
 }
-exports.closePort = function () {
+exports.closePort = function() {
     port.close();
 }
