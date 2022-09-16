6a3d377a512235746bd1ebbca8221d86b2fd4a40
Mathias Illian <heavymatill@gmail.com>
2022-09-16 15:53:18 +0200

    fixed restart api

--- routes/restart.js
+++ routes/restart.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var socketApi = require('../socketApi');
-var io = socketApi.io;
+const { spawn } = require('child_process');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log("restart triggerd via web")
-    io.emit('restart', 'Secret');
+    console.log("Restarting process.");
+    spawn(process.argv[1], process.argv.slice(2)).unref()
+    process.exit()
     res.send('restart');
 });
 module.exports = router;
